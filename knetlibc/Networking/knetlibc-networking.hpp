2249915ade60f0058c316bdd338e46998de95a48
Kanishk Jain <kanishkjain03082005@gmail.com>
2025-10-25 22:44:13 +0530

    Listening Socket

---
+++ knetlibc/Networking/knetlibc-networking.hpp
@@ -0,0 +1,10 @@
+// knetlibc-networking.hpp
+
+#ifndef knetlibc_networking_hpp
+#define knetlibc_networking_hpp
+
+#include <stdio.h>
+
+#include "Sockets/knetlibc-sockets.hpp"
+
+#endif /* knetlibc_networking_hpp */
---
+++ knetlibc/knetlibc.hpp
@@ -0,0 +1,10 @@
+// knetlibc.hpp
+
+#ifndef knetlibc_hpp
+#define knetlibc_hpp
+
+#include <stdio.h>
+
+#include "Networking/knetlibc-networking.hpp"
+
+#endif /* knetlibc_networking_hpp */
